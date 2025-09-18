5529b22dd8c2dc09df341c52df88b03201e46e88
lg1033338 <lg1033338@otc.edu>
2025-09-18 10:01:42 -0500

    My very cool second program
    Tells you if its odd or even depending on the number you give it!

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/* Divison Program
+Created By: Logan Galindo
+Takes in two numbers and divides 
+*/
+
+// Ask User for # 
+Console.Write("Enter a number: ");
+
+// Take in a string and convert
+int x = Convert.ToInt32(Console.ReadLine());
+
+// Using Mod to check if Even
+if (x % 2 == 0)
+{
+    // If number was even then show "Even" to User
+    Console.WriteLine("Even");
+}
+// Uses Mod to check if Odd
+else if (x % 1 == 0)
+{
+    // If number was odd then show "Odd" to User
+    Console.WriteLine("Odd");
+}
